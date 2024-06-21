eca687cfe30d7249c0f8b2510d6e02a3e05925e1
security-camera <lexaryab2010@gmail.com>
2024-06-21 23:51:36 +0300

    Добавьте файлы проекта.

---
+++ PinkCandy/Config.cs
@@ -0,0 +1,12 @@
+﻿using Exiled.API.Interfaces;
+namespace PinkCandy
+{
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+        public bool Debug { get; set; } = true;
+        public int Chance { get; set; } = 15;
+    }
+}
+
+
---
+++ PinkCandy/Plugin.cs
@@ -0,0 +1,30 @@
+﻿using Exiled.API.Enums;
+using Exiled.API.Features;
+using UnityEngine;
+using Scp330 = InventorySystem.Items.Usables.Scp330.CandyKindID;
+
+namespace PinkCandy
+{
+    public class Plugin : Plugin<Config>
+    {
+        public static Plugin plugin;
+        public override string Prefix => "PinkCandy";
+        public override string Name => "PinkCandy";
+        public override string Author => "[OPENSOURCE PLUGIN] [https://github.com/scp-sl-opensource-plugins]";
+        public override PluginPriority Priority => PluginPriority.Last;
+        public override void OnEnabled()
+        {
+            plugin = this;
+            Exiled.Events.Handlers.Scp330.InteractingScp330 += InteractingScp330;
+            base.OnEnabled();
+
+        }
+        private void InteractingScp330(Exiled.Events.EventArgs.Scp330.InteractingScp330EventArgs ev) { if (ev.IsAllowed && Random.Range(1, 100) <= Config.Chance) ev.Candy = Scp330.Pink; }
+        public override void OnDisabled()
+        {
+            plugin = null;
+            Exiled.Events.Handlers.Scp330.InteractingScp330 -= InteractingScp330;
+            base.OnDisabled();
+        }
+    }
+}
